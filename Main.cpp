9c131c161aa40149f49c8f31089fb3d184460366
StronDay <76127547+MaksNovokhatsy@users.noreply.github.com>
2021-05-01 20:07:42 +0300

    First commit

---
+++ Main.cpp
@@ -0,0 +1,9 @@
+// Новохатский Максим КМБО-05-20
+// Вершины
+#include "Tops.h"
+#include "triangle.h"
+
+int main()
+{
+    return 0;
+}
---
+++ Tops.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Tops
+{
+private:
+
+    double _x;
+    double _y;
+    double _z;
+
+public:
+
+    Tops(const double x, const double y, const double z);
+};
